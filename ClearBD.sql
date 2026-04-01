014fc39fab1316263b869755beb86a742a58733e
Artyom Abramovsky <artyom.abramovskij@mail.ru>
2026-04-02 01:50:41 +0700

    Added SQL script to clear BD

---
+++ ClearBD.sql
@@ -0,0 +1,29 @@
+﻿USE InternetProviderDB;
+GO
+
+BEGIN TRY
+    BEGIN TRANSACTION;
+
+    DELETE FROM dbo.Payments;
+    DELETE FROM dbo.Equipment;
+    DELETE FROM dbo.Requests;
+    DELETE FROM dbo.Contracts;
+    DELETE FROM dbo.Tariffs;
+    DELETE FROM dbo.Clients;
+
+    DBCC CHECKIDENT ('dbo.Payments', RESEED, 0);
+    DBCC CHECKIDENT ('dbo.Equipment', RESEED, 0);
+    DBCC CHECKIDENT ('dbo.Requests', RESEED, 0);
+    DBCC CHECKIDENT ('dbo.Contracts', RESEED, 0);
+    DBCC CHECKIDENT ('dbo.Tariffs', RESEED, 0);
+    DBCC CHECKIDENT ('dbo.Clients', RESEED, 0);
+
+    COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRANSACTION;
+
+    THROW;
+END CATCH;
+GO
